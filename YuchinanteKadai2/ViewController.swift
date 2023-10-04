5c043bd427b4759a592a498a1cc5df576719fcd5
yuya-3110 <ysuu45143745@gmail.com>
2023-10-04 13:25:49 +0900

    レビュー後修正

--- YuchinanteKadai2/ViewController.swift
+++ YuchinanteKadai2/ViewController.swift
@@ -10,25 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     // テキストフィールド、セグメンテッドコントロール、ラベルのIBOutlet
-    @IBOutlet weak var textField1: UITextField!
-    @IBOutlet weak var textField2: UITextField!
-    @IBOutlet weak var segmentedControl: UISegmentedControl!
-    @IBOutlet weak var resultlabel: UILabel!
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var segmentedControl: UISegmentedControl!
+    @IBOutlet private weak var resultlabel: UILabel!
 
     // 計算ボタンが押されたときに呼び出されるアクション
-    @IBAction func calculationButton(_ sender: Any) {
+    @IBAction private func calculationButton(_ sender: Any) {
         // テキストフィールドの値を数値に変換
         let value1 = Int(textField1.text ?? "") ?? 0
         let value2 = Int(textField2.text ?? "") ?? 0
-
-        // 選択されたセグメントのインデックスを取得
-        let segmentCalculation = segmentedControl.selectedSegmentIndex
 
         // 計算結果を格納する変数
         let result: String
 
         // セグメントに基づいて計算を実行
-        switch segmentCalculation {
+        switch segmentedControl.selectedSegmentIndex {
         case 0:
             result = String(value1 + value2)
         case 1:
@@ -43,12 +40,10 @@ class ViewController: UIViewController {
                 result = String(value1 / value2)
             }
         default:
-            result = "計算不可"
+            fatalError("selectedSegmentIndex is invaild.")
         }
 
         // 計算結果をラベルに表示
         resultlabel.text = result
     }
-
-
 }
